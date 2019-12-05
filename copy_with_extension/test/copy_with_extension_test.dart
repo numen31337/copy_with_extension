52e97a6e67c761c021b5b6ceec951264d76398ad
Olekandr Kirichenko <numen31337@gmail.com>
2019-12-05 19:39:17 +0100

    copy_with_extension package

---
+++ copy_with_extension/test/copy_with_extension_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:copy_with_extension/copy_with_extension.dart';
+
+void main() {
+  test('annotation exists', () {
+    final annotation = const CopyWith();
+    expect(annotation != null, true);
+  });
+}
