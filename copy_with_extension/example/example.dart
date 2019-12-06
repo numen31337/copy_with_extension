bab2816e77b754ef7f63859e2ce0bfb57a439ae1
Alexander Kirsch <spam-reporter-3000@alexander-kirsch.com>
2019-12-06 16:46:05 +0100

    copy_with_extension package corrections

---
+++ copy_with_extension/example/example.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart' show immutable;
+import 'package:copy_with_extension/copy_with_extension.dart';
+
+/// Make sure that `part` is specified, even before launching the builder
+part 'example.g.dart';
+
+@immutable
+@CopyWith()
+class SimpleObject {
+  final String id;
+  final int value;
+
+  /// Make sure that constructor has named parameters (wrapped in curly braces)
+  SimpleObject({this.id, this.value});
+}
