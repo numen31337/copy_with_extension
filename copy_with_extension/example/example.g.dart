3c92c7367c9cc764b96207e3baffa9edbd3e1cd5
Olekandr Kirichenko <numen31337@gmail.com>
2019-12-06 16:46:05 +0100

    copy_with_extension package corrections

---
+++ copy_with_extension/example/example.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+extension CopyWithExtension on SimpleObject {
+  SimpleObject copyWith({
+    String id,
+    int value,
+  }) {
+    return SimpleObject(
+      id: id ?? this.id,
+      value: value ?? this.value,
+    );
+  }
+}
--- copy_with_extension/lib/copy_with_extension.dart
+++ copy_with_extension/lib/copy_with_extension.dart
@@ -1,3 +1,9 @@
+/// Provides annotation class to use with
+/// [copy_with_extension_gen](https://pub.dev/packages/copy_with_extension_gen).
+library copy_with_extension;
+
+/// Annotation used to indicate that the `copyWith` extension should be generated.
 class CopyWith {
+  /// Creates a new [CopyWith] instance.
   const CopyWith();
 }
