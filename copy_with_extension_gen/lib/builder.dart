0beeb13a0675e40206a4005b5755a7fda5371f69
Olekandr Kirichenko <numen31337@gmail.com>
2019-12-06 17:11:01 +0100

    copy_with_extension_gen package corrections

--- copy_with_extension_gen/lib/builder.dart
+++ copy_with_extension_gen/lib/builder.dart
@@ -1,7 +1,21 @@
+/// Configuration for using `package:build`-compatible build systems.
+///
+/// See:
+/// * [build_runner](https://pub.dev/packages/build_runner)
+///
+/// This library is **not** intended to be imported by typical end-users unless
+/// you are creating a custom compilation pipeline. See documentation for
+/// details, and `build.yaml` for how these builders are configured by default.
+library copy_with_extension_gen.builder;
+
 import 'package:build/build.dart' show Builder, BuilderOptions;
 import 'package:source_gen/source_gen.dart' show SharedPartBuilder;
 import 'package:copy_with_extension_gen/src/copy_with_generator.dart';
 
+/// Supports `package:build_runner` creation and configuration of
+/// `copy_with_extension_gen`.
+///
+/// Not meant to be invoked by hand-authored code.
 Builder copyWith(BuilderOptions _) => SharedPartBuilder(
       [CopyWithGenerator()],
       'copyWith',
