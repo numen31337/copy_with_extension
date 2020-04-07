702d6c8ce512086ab6559f2d51ca19999e978d88
Olekandr Kirichenko <numen31337@gmail.com>
2020-04-07 19:26:53 +0200

    Adding a simple project for the internal testing purposes

---
+++ copy_with_extension_test/lib/basic_class.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'basic_class.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+// ignore_for_file: argument_type_not_assignable, implicit_dynamic_type, always_specify_types
+
+extension BasicClassCopyWithExtension on BasicClass {
+  BasicClass copyWith({
+    String id,
+  }) {
+    return BasicClass(
+      id: id ?? this.id,
+    );
+  }
+}
