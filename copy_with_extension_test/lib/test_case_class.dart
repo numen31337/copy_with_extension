9fa62fd696b307f4ce2adb699e0c809372b20c91
Oleksandr Kirichenko <contact@oleksandrkirichenko.com>
2020-10-25 11:52:36 +0100

    enabling the analyser strong mode

--- copy_with_extension_test/lib/test_case_class.dart
+++ copy_with_extension_test/lib/test_case_class.dart
@@ -3,10 +3,10 @@ import 'package:copy_with_extension/copy_with_extension.dart';
 part 'test_case_class.g.dart';
 
 @CopyWith(generateCopyWithNull: true)
-class Test_Case_Class<T extends String> {
+class TestCaseClass<T extends String> {
   final T id;
   @CopyWithField(immutable: true)
   final int immutableField;
 
-  Test_Case_Class({this.id, this.immutableField});
+  TestCaseClass({this.id, this.immutableField});
 }
