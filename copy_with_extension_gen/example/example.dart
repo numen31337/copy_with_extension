a1e0841663bb21a2b54557144036cfe1b23cca47
Oleksandr Kirichenko <contact@oleksandrkirichenko.com>
2021-12-20 21:03:40 +0100

    a better example file

--- copy_with_extension_gen/example/example.dart
+++ copy_with_extension_gen/example/example.dart
@@ -1,15 +1,39 @@
 import 'package:meta/meta.dart' show immutable;
 import 'package:copy_with_extension/copy_with_extension.dart';
 
-/// Make sure that `part` is specified, even before launching the builder
-/// part 'example.g.dart';
+/// Make sure the `part` is specified before running the builder.
+/// part 'example.g.dart'; /// It should not be commented.
 
+/// Lets use it like this: `SimpleObject(id: "test").copyWith.id("new value")`.
 @immutable
 @CopyWith()
 class SimpleObject {
   final String id;
-  final int value;
+  final int? intValue;
 
   /// Make sure that constructor has named parameters (wrapped in curly braces)
-  const SimpleObject({required this.id, required this.value});
+  const SimpleObject({required this.id, this.intValue});
+}
+
+/// Lets use it like this: `SimpleObject(id: "test").copyWithValues(id: "new values", intValue: 10).copyWithNull(intValue: true)`.
+@immutable
+@CopyWith(copyWith: false, copyWithNull: true, copyWithValues: true)
+class SimpleObjectOldStyle {
+  final String id;
+  final int? intValue;
+
+  /// Make sure that constructor has named parameters (wrapped in curly braces)
+  const SimpleObjectOldStyle({required this.id, this.intValue});
+}
+
+/// Will not allow you to copy with modified `id` field after object creation.
+@immutable
+@CopyWith(copyWith: false, copyWithNull: true, copyWithValues: true)
+class SimpleObjectImmutableField {
+  @CopyWithField(immutable: true)
+  final String? id;
+  final int? intValue;
+
+  /// Make sure that constructor has named parameters (wrapped in curly braces)
+  const SimpleObjectImmutableField({this.id, this.intValue});
 }
