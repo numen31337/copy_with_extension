0afddfaf7aa918bd49d888911e9077cf2a912c8d
Oleksandr Kirichenko <contact@oleksandrkirichenko.com>
2022-08-03 22:33:59 +0200

    Namespace support

--- copy_with_extension_gen/lib/src/field_info.dart
+++ copy_with_extension_gen/lib/src/field_info.dart
@@ -1,8 +1,9 @@
 import 'package:analyzer/dart/constant/value.dart' show DartObject;
 import 'package:analyzer/dart/element/element.dart'
-    show ClassElement, FieldElement, ParameterElement;
+    show ClassElement, FieldElement, ParameterElement, PrefixElement;
 import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:copy_with_extension/copy_with_extension.dart';
+import 'package:copy_with_extension_gen/src/helpers.dart';
 import 'package:source_gen/source_gen.dart' show ConstantReader, TypeChecker;
 
 /// Represents a single class field with the additional metadata needed for code generation.
@@ -14,13 +15,27 @@ class FieldInfo {
 
   FieldInfo(ParameterElement element, ClassElement classElement)
       : name = element.name,
-        type = element.type.getDisplayString(withNullability: true),
+        type = _fullTypeName(element),
         immutable = _readFieldAnnotation(element, classElement).immutable,
         nullable = element.type.nullabilitySuffix != NullabilitySuffix.none;
 
   @override
   String toString() {
     return 'type:$type name:$name immutable:$immutable nullable:$nullable';
+  }
+
+  /// Returns full type name including namespace.
+  static String _fullTypeName(ParameterElement element) {
+    final displayName = element.type.getDisplayString(withNullability: true);
+
+    final prefix = element.library?.prefixes.safeFirst;
+    final prefixIsFromTheCorrectLibrary =
+        prefix?.library.id != element.type.element?.library?.id;
+    final namespace = prefix is PrefixElement && prefixIsFromTheCorrectLibrary
+        ? prefix.name
+        : null;
+
+    return namespace is String ? "$namespace.$displayName" : displayName;
   }
 
   /// Restores the `CopyWithField` annotation provided by the user.
