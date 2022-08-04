e07fff8a751f157a09319e5e0301764553c85b2f
Alexander Kirsch <spam-reporter-3000@alexander-kirsch.com>
2022-08-04 17:12:03 +0200

    better test

--- copy_with_extension_gen/test/gen_namespace_test.dart
+++ copy_with_extension_gen/test/gen_namespace_test.dart
@@ -1,23 +1,30 @@
-import 'dart:ffi';
-import 'dart:typed_data' as td;
+import 'dart:typed_data' as ns;
+import 'dart:typed_data' as ns1;
+import 'dart:async' as ns2;
 
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:test/test.dart';
 
 part 'gen_namespace_test.g.dart';
 
-class ByteBuffer {}
+class ByteBuffer {
+  const ByteBuffer();
+}
 
 @CopyWith()
 class NamespaceTestClass {
-  final td.ByteBuffer? namespacedProperty;
-  final ByteBuffer? regularProperty;
+  final ns.ByteBuffer? namespacedProperty;
+  final ns1.ByteBuffer? namespacedProperty1;
+  final ns2.Future<ns1.ByteBuffer>? namespacedProperty2;
+  final ByteBuffer regularProperty;
   final String? stringProperty;
   final bool? boolProperty;
 
   const NamespaceTestClass({
     this.namespacedProperty,
-    this.regularProperty,
+    this.namespacedProperty1,
+    this.namespacedProperty2,
+    this.regularProperty = const ByteBuffer(),
     this.stringProperty,
     this.boolProperty,
   });
