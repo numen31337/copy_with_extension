dfd2995471475e3eda9ce1b8fc3f505b17a452ff
Oleksandr Kirichenko <5677845+numen31337@users.noreply.github.com>
2022-08-05 13:17:56 +0200

    private extensions for private classes (#61)

--- copy_with_extension_gen/test/generated_code_test_cases/test_case_copy_with_null.dart
+++ copy_with_extension_gen/test/generated_code_test_cases/test_case_copy_with_null.dart
@@ -1,24 +1,25 @@
 part of 'source_gen_entrypoint.dart';
 
 @ShouldGenerate(r'''
-abstract class _$WithNullableWithoutFieldsCWProxy<T extends Iterable<int>> {
+abstract class _$_PrivateWithNullableWithoutFieldsCWProxy<
+    T extends Iterable<int>> {
   /// This function **does support** nullification of nullable fields. All `null` values passed to `non-nullable` fields will be ignored.
   ///
   /// Usage
   /// ```dart
-  /// WithNullableWithoutFields<T>(...).copyWith(id: 12, name: "My name")
+  /// _PrivateWithNullableWithoutFields<T>(...).copyWith(id: 12, name: "My name")
   /// ````
-  WithNullableWithoutFields<T> call({
+  _PrivateWithNullableWithoutFields<T> call({
     T? nullable,
   });
 }
 
-/// Proxy class for `copyWith` functionality. This is a callable class and can be used as follows: `instanceOfWithNullableWithoutFields.copyWith(...)`.
-class _$WithNullableWithoutFieldsCWProxyImpl<T extends Iterable<int>>
-    implements _$WithNullableWithoutFieldsCWProxy<T> {
-  final WithNullableWithoutFields<T> _value;
+/// Proxy class for `copyWith` functionality. This is a callable class and can be used as follows: `instanceOf_PrivateWithNullableWithoutFields.copyWith(...)`.
+class _$_PrivateWithNullableWithoutFieldsCWProxyImpl<T extends Iterable<int>>
+    implements _$_PrivateWithNullableWithoutFieldsCWProxy<T> {
+  final _PrivateWithNullableWithoutFields<T> _value;
 
-  const _$WithNullableWithoutFieldsCWProxyImpl(this._value);
+  const _$_PrivateWithNullableWithoutFieldsCWProxyImpl(this._value);
 
   @override
 
@@ -26,12 +27,12 @@ class _$WithNullableWithoutFieldsCWProxyImpl<T extends Iterable<int>>
   ///
   /// Usage
   /// ```dart
-  /// WithNullableWithoutFields<T>(...).copyWith(id: 12, name: "My name")
+  /// _PrivateWithNullableWithoutFields<T>(...).copyWith(id: 12, name: "My name")
   /// ````
-  WithNullableWithoutFields<T> call({
+  _PrivateWithNullableWithoutFields<T> call({
     Object? nullable = const $CopyWithPlaceholder(),
   }) {
-    return WithNullableWithoutFields<T>(
+    return _PrivateWithNullableWithoutFields<T>(
       nullable: nullable == const $CopyWithPlaceholder()
           ? _value.nullable
           // ignore: cast_nullable_to_non_nullable
@@ -40,31 +41,32 @@ class _$WithNullableWithoutFieldsCWProxyImpl<T extends Iterable<int>>
   }
 }
 
-extension $WithNullableWithoutFieldsCopyWith<T extends Iterable<int>>
-    on WithNullableWithoutFields<T> {
-  /// Returns a callable class that can be used as follows: `instanceOfWithNullableWithoutFields.copyWith(...)`.
+extension _$_PrivateWithNullableWithoutFieldsCopyWith<T extends Iterable<int>>
+    on _PrivateWithNullableWithoutFields<T> {
+  /// Returns a callable class that can be used as follows: `instanceOf_PrivateWithNullableWithoutFields.copyWith(...)`.
   // ignore: library_private_types_in_public_api
-  _$WithNullableWithoutFieldsCWProxy<T> get copyWith =>
-      _$WithNullableWithoutFieldsCWProxyImpl<T>(this);
+  _$_PrivateWithNullableWithoutFieldsCWProxy<T> get copyWith =>
+      _$_PrivateWithNullableWithoutFieldsCWProxyImpl<T>(this);
 
   /// Copies the object with the specific fields set to `null`. If you pass `false` as a parameter, nothing will be done and it will be ignored. Don't do it. Prefer `copyWith(field: null)`.
   ///
   /// Usage
   /// ```dart
-  /// WithNullableWithoutFields<T>(...).copyWithNull(firstField: true, secondField: true)
+  /// _PrivateWithNullableWithoutFields<T>(...).copyWithNull(firstField: true, secondField: true)
   /// ````
-  WithNullableWithoutFields<T> copyWithNull({
+  _PrivateWithNullableWithoutFields<T> copyWithNull({
     bool nullable = false,
   }) {
-    return WithNullableWithoutFields<T>(
+    return _PrivateWithNullableWithoutFields<T>(
       nullable: nullable == true ? null : this.nullable,
     );
   }
 }
 ''')
 @CopyWith(skipFields: true, copyWithNull: true)
-class WithNullableWithoutFields<T extends Iterable<int>> {
+// ignore: unused_element
+class _PrivateWithNullableWithoutFields<T extends Iterable<int>> {
   final T? nullable;
 
-  const WithNullableWithoutFields({this.nullable});
+  const _PrivateWithNullableWithoutFields({this.nullable});
 }
