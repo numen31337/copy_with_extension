b7ff8139bbf23070f155c9d6ad698daabf5395d5
Alexander Kirsch <spam-reporter-3000@alexander-kirsch.com>
2022-12-03 00:13:04 +0100

    Release 5.0.0 (#71)
    * removing global Settings object + testing global settings
    
    * sort_constructors_first
    
    * tests fix
    
    * minor corrections
    
    * version bump to 5.0.0 to avoid compatibility issues
    
    * minor corrections

--- copy_with_extension/example/example.dart
+++ copy_with_extension/example/example.dart
@@ -9,31 +9,28 @@ import 'package:copy_with_extension/copy_with_extension.dart';
 /// Or like this: `SimpleObject(id: "test").copyWith.id("new value")`.
 @CopyWith(copyWithNull: true)
 class SimpleObjectOldStyle {
+  const SimpleObjectOldStyle({required this.id, this.intValue});
+
   final String id;
   final int? intValue;
-
-  /// Make sure that constructor has named parameters (wrapped in curly braces)
-  const SimpleObjectOldStyle({required this.id, this.intValue});
 }
 
 /// Won't allow you to copy this object with a modified `id` field after object creation. It will always copy it from the original instance.
 @CopyWith()
 class SimpleObjectImmutableField {
+  const SimpleObjectImmutableField({this.id, this.intValue});
+
   @CopyWithField(immutable: true)
   final String? id;
   final int? intValue;
-
-  /// Make sure that constructor has named parameters (wrapped in curly braces)
-  const SimpleObjectImmutableField({this.id, this.intValue});
 }
 
 /// Allows the use of a private constructor.
 @CopyWith(constructor: "_")
 class SimpleObjectPrivateConstructor {
+  const SimpleObjectPrivateConstructor._({this.id, this.intValue});
+
   @CopyWithField(immutable: true)
   final String? id;
   final int? intValue;
-
-  /// Make sure that constructor has named parameters (wrapped in curly braces)
-  const SimpleObjectPrivateConstructor._({this.id, this.intValue});
 }
--- copy_with_extension_gen/lib/src/copy_with_field_annotation.dart
+++ copy_with_extension_gen/lib/src/copy_with_field_annotation.dart
@@ -1,5 +1,6 @@
 import 'package:copy_with_extension/copy_with_extension.dart';
 
+/// The internal representation of parameters entered by the library's user.
 class CopyWithFieldAnnotation implements CopyWithField {
   const CopyWithFieldAnnotation({
     required this.immutable,
